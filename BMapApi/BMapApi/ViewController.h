a40ed8261f1036aabdde019749751440c068c86f
谭云华 <tyh520life@163.com>
2017-05-24 15:40:54 +0800

    百度地图相关
    api的熟练调用

---
+++ BMapApi/BMapApi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BMapApi
+//
+//  Created by 启程 on 2017/5/24.
+//  Copyright © 2017年 tyh520life. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BMapApi/BMapApi/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BMapApi
+//
+//  Created by 启程 on 2017/5/24.
+//  Copyright © 2017年 tyh520life. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
